a89a63d7e06cfcf60257919bec8f2a748985c3a2
Pavel Borisov <pavellbor@gmail.com>
2023-07-21 10:12:12 +0300

    feat: Добавлены ховеры для ссылок и зафиксирован скролл в мобильном поиске

--- source/js/search.js
+++ source/js/search.js
@@ -1,59 +1,69 @@
 {
   const constructor = (search) => {
+    const lockBody = () => {
+      document.body.style.position = 'fixed'
+    }
+
+    const unlockBody = () => {
+      document.body.style.position = 'static'
+    }
+
     const showPopup = () => {
       if (!popup.classList.contains(POPUP_SWITCHABLE_CLASS)) {
-        popup.classList.add(POPUP_SWITCHABLE_CLASS);
+        popup.classList.add(POPUP_SWITCHABLE_CLASS)
       }
-    };
+    }
 
     const hidePopup = () => {
       if (popup.classList.contains(POPUP_SWITCHABLE_CLASS)) {
-        popup.classList.remove(POPUP_SWITCHABLE_CLASS);
+        popup.classList.remove(POPUP_SWITCHABLE_CLASS)
       }
-    };
+    }
 
     const clearInput = () => {
-      input.value = "";
-    };
+      input.value = ''
+    }
 
     const onHide = () => {
-      hidePopup();
-      clearInput();
-    };
+      hidePopup()
+      clearInput()
+      unlockBody()
+    }
 
     const onShowClick = () => {
-      showPopup();
-    };
+      showPopup()
+      lockBody()
+    }
 
     const onHideClick = () => {
-      onHide();
-    };
+      onHide()
+    }
 
     const onClearClick = () => {
-      clearInput();
-    };
+      clearInput()
+    }
 
     const onOverlayClick = (event) => {
-      const isOverlayEvent = event.target === popup;
+      const isOverlayEvent = event.target === popup
       if (isOverlayEvent) {
-        onHide();
+        onHide()
       }
-    };
+    }
 
-    const openBtn = search.querySelector("[data-search-open]");
-    const closeBtn = search.querySelector("[data-search-close]");
-    const clearBtn = search.querySelector("[data-search-clear]");
-    const popup = search.querySelector("[data-search-popup]");
-    const input = search.querySelector("[data-search-input]");
+    const openBtn = search.querySelector('[data-search-open]')
+    const closeBtn = search.querySelector('[data-search-close]')
+    const clearBtn = search.querySelector('[data-search-clear]')
+    const popup = search.querySelector('[data-search-popup]')
+    const input = search.querySelector('[data-search-input]')
 
-    const POPUP_SWITCHABLE_CLASS = "search__popup--visible";
+    const POPUP_SWITCHABLE_CLASS = 'search__popup--visible'
 
-    openBtn.addEventListener("click", onShowClick);
-    closeBtn.addEventListener("click", onHideClick);
-    clearBtn.addEventListener("click", onClearClick);
-    popup.addEventListener("click", onOverlayClick);
-  };
+    openBtn.addEventListener('click', onShowClick)
+    closeBtn.addEventListener('click', onHideClick)
+    clearBtn.addEventListener('click', onClearClick)
+    popup.addEventListener('click', onOverlayClick)
+  }
 
-  const search = document.querySelector("[data-search]");
-  constructor(search);
+  const search = document.querySelector('[data-search]')
+  constructor(search)
 }
