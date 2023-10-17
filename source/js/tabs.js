8f27da4eb654a4ee4dfb8b51eba0acfe27befa2c
Pavel Borisov <pavellbor@gmail.com>
2023-10-17 21:22:31 +0300

    Доработан скрипт переключения табов

--- source/js/tabs.js
+++ source/js/tabs.js
@@ -1,29 +1,62 @@
 {
-    const navTabs = document.querySelectorAll('[data-nav-number]')
-    const tabPanels = document.querySelectorAll('[data-tab-number]')
+  const TABS_CONTAINER_ATTRIBUTE = 'data-tabs'
+  
+  const NAV_TAB_ATTRIBUTE = 'data-tabs-nav'
+  const NAV_TAB_ACTIVE_CLASS = 'nav-tabs__item--active'
 
-    const setActiveStateControl = (navTabs, tabPanels) => {
+  const PANEL_TAB_ATTRIBUTE = 'data-tabs-panel'
+  const PANEL_TAB_HIDDEN_CLASS = 'tab-panel--hidden'
 
-        navTabs.forEach((navTab) => {
-            navTab.addEventListener('click', () => {
-
-                navTabs.forEach((tab) => {
-                    tab.classList.remove('nav-tabs__item--active')
-                })
-
-                navTab.classList.add('nav-tabs__item--active')
-                const tabNumber = navTab.getAttribute('data-nav-number')
-
-                tabPanels.forEach((panel) => {
-                    panel.classList.add('tab-panel--hidden')
-                })
-
-                const activePanel = document.querySelector(`[data-tab-number="${tabNumber}"]`)
-                activePanel.classList.remove('tab-panel--hidden')
-
-            })
-        })
+  const constructor = (container) => {
+    const hideAllPanelTabs = (panelTabs) => {
+      panelTabs.forEach((panel) => {
+        panel.classList.add(PANEL_TAB_HIDDEN_CLASS)
+      })
     }
 
-    setActiveStateControl(navTabs, tabPanels)
+    const getPanelTabByNavTab = (navTab) => {
+      const tabNumber = navTab.getAttribute(NAV_TAB_ATTRIBUTE)
+      return container.querySelector(`[${PANEL_TAB_ATTRIBUTE}="${tabNumber}"]`)
+    }
+    
+    const showPanelTab = (panelTab) => {
+      panelTab.classList.remove(PANEL_TAB_HIDDEN_CLASS)
+    }
+
+    const resetAllNavTabs = (navTabs) => {
+      navTabs.forEach((navTab) => {
+        navTab.classList.remove(NAV_TAB_ACTIVE_CLASS)
+      })
+    }
+
+    const setActiveNavTab = (navTab) => {
+      navTab.classList.add(NAV_TAB_ACTIVE_CLASS)
+    }
+
+    const updateActiveNavTab = (navTabs, navTab) => {
+      resetAllNavTabs(navTabs)
+      setActiveNavTab(navTab)
+    }
+
+    const updateActivePanelTab = (panelTabs, panelTab) => {
+      hideAllPanelTabs(panelTabs)
+      showPanelTab(panelTab)
+    }
+
+    const initEvents = (navTab) => {
+      navTab.addEventListener('click', () => {
+        updateActiveNavTab(navTabs, navTab)
+
+        const panelTab = getPanelTabByNavTab(navTab)
+        updateActivePanelTab(panelTabs, panelTab)
+      })
+    }
+
+    const navTabs = container.querySelectorAll(`[${NAV_TAB_ATTRIBUTE}]`)
+    const panelTabs = container.querySelectorAll(`[${PANEL_TAB_ATTRIBUTE}]`)
+    navTabs.forEach(initEvents)
+  }
+
+  const containers = document.querySelectorAll(`[${TABS_CONTAINER_ATTRIBUTE}]`)
+  containers.forEach(constructor)
 }
